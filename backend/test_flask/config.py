0d62c1767249b3ea6e8ea314374ff0bcfdd7fdad
MaKsY <mark.merlin2003@gmail.com>
2023-02-26 16:59:12 +0300

    my bd

--- backend/test_flask/config.py
+++
@@ -1,9 +0,0 @@
-DATABASE = {
-    'drivername': 'postgresql',
-    'host': 'localhost',
-    'port': '5432',
-    'username': 'maksim',
-    'password': 'postgres',
-    'database': 'adult_map'
-}
-SECRET_KEY = 'g0tikka'
